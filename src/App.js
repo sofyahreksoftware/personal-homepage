8527e271475452964982723b44a1ead845ae3f0c
sofyahrek <sofigrekal@gmail.com>
2024-05-01 11:48:21 +0200

    feat(profile): add skills and future skills sections for mobile and desktop

--- src/App.js
+++ src/App.js
@@ -1,3 +1,68 @@
-function App() {}
+import { ListSection } from "./ListSection";
+
+function App() {
+  const skills = [
+    "Semantic & accessible HTML",
+    "Responsive Web Design",
+    "SVG",
+    "Web Fonts",
+    "OpenGraph protocol",
+    "CSS",
+    "CSS Bem Convention",
+    "Normalize.css",
+    "box-sizing: border-box",
+    "CSS Grid",
+    "CSS Flexbox",
+    "Media Queries",
+    "CSS Selectors",
+    "Pseudoclasses and Pseudo-elements",
+    "Transitions",
+    "Media Queries",
+    "JavaScript (ES6+)",
+    "NPM",
+    "Node.js",
+    "Immutability",
+    "React.js",
+    "React Router",
+    "Styled Components",
+    "Redux (Toolkit)",
+    "Redux-Saga & generator functions ",
+    "Working with API (XMLHttpRequest, Fetch, Axios)",
+    "Promise, Async/Await",
+    "Error handling",
+    "JSON",
+    "Terminal",
+    "Git",
+    "Git branching",
+    "GitHub",
+    "Pull Requests & Review",
+    "Markdown",
+    "Scrum",
+    "Figma",
+    "Trello",
+  ];
+  const skillsToLearn = [
+    "TypeScript",
+    "Next.js",
+    "jQuery",
+    "React Context",
+    "Cypress",
+    "Unit testing",
+  ];
+
+  return (
+    <div>
+      <ListSection
+        titleName="My skillset includes&nbsp;🛠️"
+        listItems={skills}
+      />
+      <ListSection
+        titleName="What I want to learn next&nbsp;🚀"
+        listItems={skillsToLearn}
+        $condensedView
+      />
+    </div>
+  );
+}
 
 export default App;
--- src/theme.js
+++ src/theme.js
@@ -1,7 +1,12 @@
 export const theme = {
+  media: { mobile: 992 },
   colors: {
     white: "rgba(255,255,255,1)",
     whiteLilac: "rgba(251, 251, 254, 1)",
+    iron: "rgba(229, 229, 229, 1)",
+    mineShaft: "rgba(37, 37, 37, 1)",
+    cornflowerBlue: "rgba(3, 102, 214, 1)",
+    slateGray: "rgba(110, 126, 145, 1)",
     black: "rgba(0,0,0,1)",
   },
 };
