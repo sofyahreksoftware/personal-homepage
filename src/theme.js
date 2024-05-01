2a88b475af39f7823067819a10e2ad75e30fc1ec
sofyahrek <sofigrekal@gmail.com>
2024-05-01 11:21:43 +0200

    refactor(theme): change color format to RGBA

--- src/theme.js
+++ src/theme.js
@@ -1,7 +1,7 @@
 export const theme = {
   colors: {
-    white: "#FFFFFF",
-    whiteLilac: "#FBFBFE",
-    black: "#DDDDD",
+    white: "rgba(255,255,255,1)",
+    whiteLilac: "rgba(251, 251, 254, 1)",
+    black: "rgba(0,0,0,1)",
   },
 };
