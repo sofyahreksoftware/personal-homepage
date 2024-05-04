925e46c1ed6e3ed6aba7bc67bb8caae2c2a227bb
sofyahrek <sofigrekal@gmail.com>
2024-05-04 09:23:11 +0200

    refactor: move skills and skillsToLearn to separate directory

--- src/App.js
+++ src/App.js
@@ -1,60 +1,12 @@
 import { ListSection } from "./ListSection";
+import { mySkills, skillsToLearn } from "./mySkills&skillsToLearn";
 
 function App() {
-  const skills = [
-    "Semantic & accessible HTML",
-    "Responsive Web Design",
-    "SVG",
-    "Web Fonts",
-    "OpenGraph protocol",
-    "CSS",
-    "CSS Bem Convention",
-    "Normalize.css",
-    "box-sizing: border-box",
-    "CSS Grid",
-    "CSS Flexbox",
-    "Media Queries",
-    "CSS Selectors",
-    "Pseudoclasses and Pseudo-elements",
-    "Transitions",
-    "Media Queries",
-    "JavaScript (ES6+)",
-    "NPM",
-    "Node.js",
-    "Immutability",
-    "React.js",
-    "React Router",
-    "Styled Components",
-    "Redux (Toolkit)",
-    "Redux-Saga & generator functions ",
-    "Working with API (XMLHttpRequest, Fetch, Axios)",
-    "Promise, Async/Await",
-    "Error handling",
-    "JSON",
-    "Terminal",
-    "Git",
-    "Git branching",
-    "GitHub",
-    "Pull Requests & Review",
-    "Markdown",
-    "Scrum",
-    "Figma",
-    "Trello",
-  ];
-  const skillsToLearn = [
-    "TypeScript",
-    "Next.js",
-    "jQuery",
-    "React Context",
-    "Cypress",
-    "Unit testing",
-  ];
-
   return (
     <div>
       <ListSection
         titleName="My skillset includes&nbsp;🛠️"
-        listItems={skills}
+        listItems={mySkills}
       />
       <ListSection
         titleName="What I want to learn next&nbsp;🚀"
