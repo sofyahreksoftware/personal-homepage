e50898143d809bd0a3904294fab23dc3ba0b3c7f
Sofya <sofigrekal@gmail.com>
2024-05-13 12:38:10 +0200

    feat: add MainContainer styled component for main content styling

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { MainContainer } from "./MainContainer/styled";
 import { Bio } from "./Bio";
 import { SkillsSection } from "./SkillsSection";
 import { currentSkillsList, futureSkillsList } from "./skills";
@@ -5,20 +6,22 @@ import { currentSkillsList, futureSkillsList } from "./skills";
 function App() {
   return (
     <div>
-      <Bio
-        titleAdditionalContent="THIS IS"
-        titleName="Sofya Hrek"
-        description="I am a passionate Frontend Developer with a strong passion for React, actively pursuing new career opportunities."
-      />
-      <SkillsSection
-        titleName="My skillset includes&nbsp;🛠️"
-        skills={currentSkillsList}
-      />
-      <SkillsSection
-        titleName="What I want to learn next&nbsp;🚀"
-        skills={futureSkillsList}
-        $condensedView
-      />
+      <MainContainer>
+        <Bio
+          titleAdditionalContent="THIS IS"
+          titleName="Sofya Hrek"
+          description="I am a passionate Frontend Developer with a strong passion for React, actively pursuing new career opportunities."
+        />
+        <SkillsSection
+          titleName="My skillset includes&nbsp;🛠️"
+          skills={currentSkillsList}
+        />
+        <SkillsSection
+          titleName="What I want to learn next&nbsp;🚀"
+          skills={futureSkillsList}
+          $condensedView
+        />
+      </MainContainer>
     </div>
   );
 }
