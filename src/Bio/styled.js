14c09cb143c0265bac8c75692c5ac656e7f443c4
Sofya <sofigrekal@gmail.com>
2024-06-27 16:44:59 +0200

    refactor: remove HireMeLink styled component from Bio/styled.js

--- src/Bio/styled.js
+++ src/Bio/styled.js
@@ -40,31 +40,3 @@ export const Image = styled.img`
     padding: 0;
   }
 `;
-
-export const HireMeLink = styled.a`
-  display: flex;
-  gap: 16px;
-  margin: 0;
-  padding: 2px 2px 2px 16px;
-  width: 154px;
-  height: 49px;
-  border-radius: 4px;
-  white-space: nowrap;
-  background-color: ${({ theme }) => theme.colors.scienceBlue};
-  text-align: center;
-  align-items: center;
-  font-size: 20.06px;
-  font-weight: 600px;
-  text-decoration: none;
-  color: ${({ theme }) => theme.colors.white};
-  transition: background-color 2s ease-out;
-
-  &:hover {
-    background-color: ${({ theme }) => theme.colors.electricBlue};
-    cursor: pointer;
-  }
-
-  @media (max-width: ${({ theme }) => theme.media.mobile}px) {
-    margin: 24px 0;
-  }
-`;
