eb8d4fa9c26480c0278a9ef73450af6f856845ef
Sofya <sofigrekal@gmail.com>
2024-07-11 22:42:13 +0200

    refactor: move repetitive declarations from darkTheme and lightTheme to mainTheme

--- src/RepositoryCard/styled.js
+++ src/RepositoryCard/styled.js
@@ -4,10 +4,10 @@ export const StyledRepositoryCard = styled.article`
   width: 100%;
   padding: 56px;
   background-color: ${({ theme }) => theme.colors.backgrounds.section};
-  border: 6px solid ${({ theme }) => theme.colors.border};
+  border: 6px solid ${({ theme }) => theme.commonColors.mistGray};
   border-radius: 5px;
-  box-shadow: 0px 16px 58px 0px ${({ theme }) => theme.colors.shadow.internal},
-    0px -2px 50px 0px ${({ theme }) => theme.colors.shadow.external};
+  box-shadow: 0px 16px 58px 0px ${({ theme }) => theme.commonColors.violet},
+    0px -2px 50px 0px ${({ theme }) => theme.commonColors.violet2};
 
   @media (max-width: ${({ theme }) => theme.media.tablet}px) {
     padding: 24px;
