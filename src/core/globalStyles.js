21de25f6a2d1c453b523ee13ce6b741cd6f90e69
Sofya <sofigrekal@gmail.com>
2024-07-18 23:41:09 +0200

    style: formatt globalStyles.js

--- src/core/globalStyles.js
+++ src/core/globalStyles.js
@@ -1,28 +1,26 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-html {
+  html {
     box-sizing: border-box;
   }
-  *,
-  ::after,
-  ::before {
+  *, ::after, ::before {
     box-sizing: inherit;
   }
 
-body {
- max-width: 1216px;
- transition: all 0.5s ease;
- margin: 0 auto;
- padding: 0 14px;
- background-color: ${({ theme }) => theme.colors.backgrounds.main};
- font-family: "Inter", sans-serif;
- font-weight: 400px;
- letter-spacing: 1.5px;
--moz-osx-font-smoothing: grayscale;
-}
+  body {
+    max-width: 1216px;
+    transition: all 0.5s ease;
+    margin: 0 auto;
+    padding: 0 14px;
+    background-color: ${({ theme }) => theme.colors.backgrounds.main};
+    font-family: "Inter", sans-serif;
+    font-weight: 400px;
+    letter-spacing: 1.5px;
+    -moz-osx-font-smoothing: grayscale;
+  }
 
-button:hover {
-    cursor: pointer
+  button:hover {
+    cursor: pointer;
   }
 `;
