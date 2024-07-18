690714b9e34df779985967847102308b1f26422b
Sofya <sofigrekal@gmail.com>
2024-07-18 21:44:32 +0200

    refactor: adjust spacing between Footer elements

--- src/features/personalHomepage/Footer/styled.js
+++ src/features/personalHomepage/Footer/styled.js
@@ -4,22 +4,21 @@ export const StyledFooter = styled.footer`
   display: grid;
   padding: 0 8px 0 4px;
   text-align: left;
-  gap: 24px;
-  margin: 0 8px;
+  margin: 0 0 109px;
   max-width: 670px;
 
   @media (max-width: ${({ theme }) => theme.media.mobile}px) {
-    gap: 12px;
+    margin: 0 0 31px;
   }
 `;
 
 export const LinksList = styled.ul`
   list-style-type: none;
-  margin: 32px 0 0 0;
+  margin: 56px 0 0 0;
   padding: 0;
   display: flex;
   flex-direction: row;
-  gap: 25px;
+  gap: 24px;
 
   svg {
     fill: ${({ theme }) => theme.colors.icons.simple.normal};
@@ -28,5 +27,10 @@ export const LinksList = styled.ul`
     &:hover {
       fill: ${({ theme }) => theme.colors.icons.simple.highlighted};
     }
+  }
+
+  @media (max-width: ${({ theme }) => theme.media.mobile}px) {
+    gap: 16px;
+    margin: 40px 0 0 0;
   }
 `;
