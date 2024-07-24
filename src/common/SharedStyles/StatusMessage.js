cc4d21ae22a2b83989b249fb7a9d4d5b9bd1fa1b
Sofya <sofigrekal@gmail.com>
2024-07-24 11:32:23 +0200

    feat: optimize StatusMessage font-size for mobile

--- src/common/SharedStyles/StatusMessage.js
+++ src/common/SharedStyles/StatusMessage.js
@@ -1,9 +1,12 @@
 import styled from "styled-components";
 
 export const StatusMessage = styled.p`
-  font-weight: 400;
   font-size: 20px;
   line-height: 1.4;
   color: ${({ theme }) => theme.colors.text.main};
   margin: 0 auto 32px;
+
+  @media (max-width: ${({ theme }) => theme.mediaBreakpoints.mobile}px) {
+    font-size: 17px;
+  }
 `;
